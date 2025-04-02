54311a58e82572a4c01a6f42a3ee7d3ed8708e8e
BL4Z3@hyperbyte.io <lahiru.r@hyperbyte.io>
2025-04-02 16:30:54 +0530

    refactor AppNavigator to use AuthContext for authentication state

--- Navigation/AppNavigator.tsx
+++ Navigation/AppNavigator.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 
-import { useAuth } from "../hooks/useAuth";
 import { AppStack } from "./AppStack";
 import { AuthStack } from "./AuthStack";
+import { useAuth } from "../Contexts/AuthContext";
 
 function AppNavigator() {
 	const { isAuthenticated } = useAuth();
